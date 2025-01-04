af0917b96b858eb750af1fbb381f63ef7965a3fc
farzin312 <farzinshifat@gmail.com>
2025-01-04 18:48:33 -0500

    total

--- src/components/Payment.js
+++ src/components/Payment.js
@@ -1,8 +1,8 @@
 export default function Payment({ bill, tip }) {
-
+    const total = bill + tip
     return(
         <h2>
-            You pay $X (${bill} + ${tip} tip) 
+            You pay ${total} (${bill} + ${tip} tip) 
         </h2>
     )
 }
